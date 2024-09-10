b412cec13257523d56ce1390d3cab7746deac350
okusamaOwO <lanhuong2k4UwU@gmail.com>
2024-09-10 12:01:07 +0700

    obfuscation detector

---
+++ Obfuscation detector/check.py
@@ -0,0 +1,8 @@
+import obfuscation_detection as od
+
+oc = od.ObfuscationClassifier(od.PlatformType.ALL)
+commands = ['cmd.exe /c "echo Invoke-DOSfuscation"',
+            'cm%windir:~ -4, -3%.e^Xe,;^,/^C",;,S^Et ^^o^=fus^cat^ion&,;,^se^T ^ ^ ^B^=o^ke-D^OS&&,;,s^Et^^ d^=ec^ho I^nv&&,;,C^Al^l,;,^%^D%^%B%^%o^%"',
+            'cat /etc/passwd']
+classifications = oc(commands)
+print(classifications)
